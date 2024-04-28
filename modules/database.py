7f7cfc463211960f2019759c1f45a7aa178285bf
Kevin Araújo <11686566+KevinMAraujo@users.noreply.github.com>
2024-04-28 16:42:58 -0300

    Update database.py

--- modules/database.py
+++ modules/database.py
@@ -13,40 +13,35 @@ HOST = os.environ["HOST"]
 PORT = os.environ["PORT"]
 
 
-def engine_db(database:str=None):
-    '''
-    '''
-    connection_string = f'postgresql://{USER}:{PASSWD}@{HOST}:{PORT}'
-    
-    if database is not None:
-        connection_string+=f'/{database}'
-    
-    return create_engine(connection_string)
+def engine_db():
+    engine = create_engine(f'postgresql://{USER}:{PASSWD}@{HOST}:{PORT}/{DB_NAME}')
+    return engine
+
 
 def connnection_db():
     engine = engine_db()
     return engine.connect()
-    
 
-def criar_database(database:str):
+
+def criar_database():
     try:
         engine = create_engine(f'postgresql://{USER}:{PASSWD}@{HOST}:{PORT}')
 
         with engine.connect().execution_options(isolation_level="AUTOCOMMIT") as conn:
 
             # Verifica se o banco de dados já existe
-            sql = f"SELECT 1 FROM pg_database WHERE datname = lower('{database}')"
+            sql = f"SELECT 1 FROM pg_database WHERE datname = lower('{DB_NAME}')"
             database_exists = conn.execute(text(sql)).fetchone()
 
             if not database_exists:
-                sql_create_db = f"CREATE DATABASE {database}"
+                sql_create_db = f"CREATE DATABASE {DB_NAME}"
                 conn.execute(text(sql_create_db))
                 conn.commit()
 
                 conn.close()
-                print(f"Banco de dados {database} criado com sucesso.")
+                print(f"Banco de dados {DB_NAME} criado com sucesso.")
             else:
-                print(f"O banco de dados {database} já existe.")
+                print(f"O banco de dados {DB_NAME} já existe.")
     except OperationalError as error:
         print("Erro de conexão:", error)
         raise error
@@ -55,27 +50,37 @@ def criar_database(database:str):
         raise error
 
 
-def criar_schemas(database:str):
+def criar_schemas():
     try:
-        engine = engine_db(database=database)
+        engine = engine_db()
         with engine.connect().execution_options(isolation_level="AUTOCOMMIT") as conn:
+
+            sql_exists_landing = "SELECT schema_name FROM information_schema.schemata WHERE schema_name = 'landing'"
+            exists_landing = conn.execute(text(sql_exists_landing)).fetchone()
+            if not exists_landing:
+                sql_create_landing = "CREATE SCHEMA landing"
+                conn.execute(text(sql_create_landing))
+                conn.commit()
+                print("Schema 'bronze' criado com sucesso.")
+            else:
+                print("O Schema 'bronze' ja existe.")
+
             sql_exists_bronze = "SELECT schema_name FROM information_schema.schemata WHERE schema_name = 'bronze'"
             exists_bronze = conn.execute(text(sql_exists_bronze)).fetchone()
             if not exists_bronze:
                 sql_create_bronze = "CREATE SCHEMA bronze"
                 conn.execute(text(sql_create_bronze))
-                #conn.commit()
+                conn.commit()
                 print("Schema 'bronze' criado com sucesso.")
             else:
                 print("O Schema 'bronze' ja existe.")
-
 
             sql_exists_silver = "SELECT schema_name FROM information_schema.schemata WHERE schema_name = 'silver'"
             exists_silver = conn.execute(text(sql_exists_silver)).fetchone()
             if not exists_silver:
                 sql_create_silver = "CREATE SCHEMA silver"
                 conn.execute(text(sql_create_silver))
-                # conn.commit()
+                conn.commit()
                 print("Schema 'silver' criado com sucesso.")
             else:
                 print("O Schema 'silver' ja existe.")
@@ -85,7 +90,7 @@ def criar_schemas(database:str):
             if not exists_gold:
                 sql_create_gold = "CREATE SCHEMA gold"
                 conn.execute(text(sql_create_gold))
-                # conn.commit()
+                conn.commit()
                 print("Schema 'gold' criado com sucesso.")
             else:
                 print("O Schema 'gold' ja existe.")
@@ -97,15 +102,16 @@ def criar_schemas(database:str):
         print("Ocorreu um erro:", error)
         raise error
 
-def criar_tabela_df(database:str,schema:str, nome_tabela:str, df:pd.DataFrame, if_exists:str='append'):
-    try:        
-        engine = engine_db(database=database)
-        
+
+def criar_tabela_df(schema: str, nome_tabela: str, df: pd.DataFrame, if_exists: str = 'append'):
+    try:
+        engine = engine_db()
+
         print(f"Criando a tabela '{nome_tabela}' no schema '{schema}'....")
-        
-        df.to_sql(nome_tabela, engine, schema=schema, if_exists=if_exists, index=False) 
-        
+
+        df.to_sql(nome_tabela, engine, schema=schema, if_exists=if_exists, index=False)
+
         print(f"A tabela '{nome_tabela}' foi criada no schema '{schema}', e os dados foram inseridos.")
-        
+
     except Exception as error:
         raise error
